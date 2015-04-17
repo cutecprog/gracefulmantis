d3256c6c776f932e0ab44e2f1e4c81f69d39d7aa
cutecprog <andi.grooms@gmail.com>
2015-04-17 16:03:29 -0500

    Initialize and define World BitSet object. Set respective bit in World object on writeCell function call

--- color.js
+++ color.js
@@ -1,6 +1,7 @@
 var content = document.getElementById("window");
 var ctx = document.getElementById('content').getContext('2d');
 var disp = document.getElementById('data');
+var World = new BitSet(160000);
 var time_inactive = 0;
 content.scrollLeft = 50;
 content.scrollTop = 50;
@@ -20,6 +21,7 @@ function writeCell(state,x,y)
                 ctx.rect(x+xoffset, y+yoffset, 1, 1);
                 ctx.rect(x, y+yoffset, 1, 1);
                 ctx.rect(x+xoffset, y, 1, 1);
+                World.set(y*400+x, 1);
         } else {
                 ctx.clearRect(x, y, 1, 1);
                 ctx.clearRect(x+xoffset, y+yoffset, 1, 1);
@@ -38,16 +40,26 @@ function readCell(x,y)
 function neighbors(x,y)
 {
         n = 0;
-        for(i=-1; i<2; i++)
-                for(j=-1; j<2; j++)
-                        if(readCell(x+i, y+j)[0])
-                                n += 1;
+	for(i=-1; i<2; i++)
+		if(readCell(x+i,y-1)[0])
+			alert(i+' -1');
+			n++;
+	if(readCell(x-1,y)[0])
+		alert('-1 0');
+		n++;
+	if(readCell(x+1,y)[0])
+		alert('1 0');
+		n++;
+	for(i=-1; i<2; i++)
+		if(readCell(x+i,y+1)[0])
+                        alert(i+' +1');
+                        n++;
         return n;
 }
 
 function writeDisp(str)
 {
-	disp.innerHTML = '<p>' + str + '</p>';
+        disp.innerHTML = '<p>' + str + '</p>';
 }
 
 ctx.beginPath();
@@ -58,19 +70,21 @@ ctx.beginPath();
 }*/
 writeCell(true, 20,20);
 writeCell(true, 21,20);
-writeCell(true, 20,22);
-writeCell(true, 24,20);
-writeCell(true, 20,21);
+writeCell(true, 20,19);
+writeCell(true, 23,20);
 ctx.strokeStyle="#FF7300";
-ctx.stroke();
-writeDisp('bye');
-
-setInterval(function() {
-        /*disp.innerHTML = '<p>' + content.scrollLeft + ', ' + content.scrollTop
-                       + ': ' +  neighbors(20,20) + ' ' + r+g+b + '</p>';*/
-}, 50);
-/*alert(neighbors(50,50) + ' ' + readCell(50,50)[0] + readCell(50,50)[1] +
-readCell(50,50)[2]);
+ctx.stroke();/*
+writeDisp('hi');
+for(i=17; i<23; i++) {
+        for(j=17; j<23; j++) {
+                d = readCell(i,j);
+                r = ('0' + d[0].toString(16)).slice(-2);
+                g = ('0' + d[1].toString(16)).slice(-2);
+                b = ('0' + d[2].toString(16)).slice(-2);
+                alert(i+', '+j+': '+r+g+b);
+        }
+}
+writeDisp(neighbors(21,21));
 */
 setInterval(function() {
         if (content.scrollLeft <= 0) {
