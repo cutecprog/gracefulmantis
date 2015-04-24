b46ad747d9fed47b73366a25e2bb1fcb2c8a441f
cutecprog <andi.grooms@gmail.com>
2015-04-24 06:48:55 -0500

    Initialize and define blurColors function. Add background shadow to content div element. Change background-color of body element to black, instead of off black, to improve look of shadow

--- diet.js
+++ diet.js
@@ -17,6 +17,7 @@ var minIntake = {
 };
 
 var carbs, protein, fat, cal;
+var r,g,b, rs, gs, bs;
 document.getElementById('carbs').value = '0';
 document.getElementById('protein').value = '0';
 document.getElementById('fat').value = '0';
@@ -24,6 +25,11 @@ document.getElementById('cal').value = '2400';
 
 document.getElementById('data').innerHTML = '<p>Fat: ' + caloriesPerGram.fat
                          + ' ' + maxIntake.fat + '</p>';
+
+function blurColors(c1, c2)
+{
+        return Math.sqrt((Math.pow(c1,2) + Math.pow(c2,2))/2);
+}
 
 setInterval(function()
 {
@@ -34,6 +40,8 @@ setInterval(function()
                 carbs   = (carbs * caloriesPerGram.carbs / cal);
                 protein = (protein * caloriesPerGram.protein / cal);
                 fat     = (fat * caloriesPerGram.fat / cal);
+                document.getElementById('data').innerHTML = 
+                        '<p>'+carbs+' '+protein+' '+fat+'</p>';
                 carbs   -= (minIntake.carbs);
                 protein -= (minIntake.protein);
                 fat     -= (minIntake.fat);
@@ -43,8 +51,6 @@ setInterval(function()
                 r = carbs;
                 g = protein;
                 b = fat;
-                document.getElementById('data').innerHTML = 
-                        '<p>'+r+' '+g+' '+b+'</p>';
                 r *= 256;
                 g *= 256;
                 b *= 256;
@@ -57,12 +63,26 @@ setInterval(function()
                 r = (r > 255) ? 255 : (r < 0) ? 0 : r;
                 g = (g > 255) ? 255 : (g < 0) ? 0 : g;
                 b = (b > 255) ? 255 : (b < 0) ? 0 : b;
+                rs = blurColors(r, 0);
+                gs = blurColors(g, 0);
+                bs = blurColors(b, 0);
                 r = Math.round(r).toString(16);
                 g = Math.round(g).toString(16);
                 b = Math.round(b).toString(16);
                 r = ('0' + r).substr(-2);
                 g = ('0' + g).substr(-2);
                 b = ('0' + b).substr(-2);
+                rs = (rs > 255) ? 255 : (rs < 0) ? 0 : rs;
+                gs = (gs > 255) ? 255 : (gs < 0) ? 0 : gs;
+                bs = (bs > 255) ? 255 : (bs < 0) ? 0 : bs;
+                rs = Math.round(rs).toString(16);
+                gs = Math.round(gs).toString(16);
+                bs = Math.round(bs).toString(16);
+                rs = ('0' + rs).substr(-2);
+                gs = ('0' + gs).substr(-2);
+                bs = ('0' + bs).substr(-2);
                 document.getElementById('content').style.backgroundColor = 
-                '#' + r+g+b;
+                                '#' + r+g+b;
+                document.getElementById('content').style.boxShadow = 
+                                '0 0 30px #'+rs+gs+bs;
 }, 100);
