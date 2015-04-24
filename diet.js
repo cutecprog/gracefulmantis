48cdee8e97ad05f54e760bd5df8924f53a2850c5
cutecprog <andi.grooms@gmail.com>
2015-04-23 19:50:45 -0500

    Define form that contains carbs, protein, and fat input elements. Define interval function to write value of fat input element to data div element

--- diet.js
+++ diet.js
@@ -1,14 +1,20 @@
 var caloriesPerGram = {
-        'carb':    4,
+        'carbs':   4,
         'protein': 4,
         'fat':     9
 };
 
 var maxIntake = {
-        'carb':    0.65,
+        'carbs':   0.65,
         'protein': 0.35,
         'fat':     0.35
 };
 
 document.getElementById('data').innerHTML = '<p>Fat: ' + caloriesPerGram.fat
                          + ' ' + maxIntake.fat + '</p>';
+
+setInterval(function()
+{
+        document.getElementById('data').innerHTML = '<p>Fat: '
+                + document.getElementById('fat').value + '</p>';
+}, 100);
