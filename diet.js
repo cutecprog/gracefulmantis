b461221424c0fcf01e6b3e48a94ce05e156f124e
cutecprog <andi.grooms@gmail.com>
2015-04-23 21:06:24 -0500

    Convert carbs, protein, and fat variables into value btween 0-255 and write to data div element

--- diet.js
+++ diet.js
@@ -9,12 +9,27 @@ var maxIntake = {
         'protein': 0.35,
         'fat':     0.35
 };
+var carbs, protein, fat, cal;
+document.getElementById('cal').value = '2400';
 
 document.getElementById('data').innerHTML = '<p>Fat: ' + caloriesPerGram.fat
                          + ' ' + maxIntake.fat + '</p>';
 
 setInterval(function()
 {
-        document.getElementById('data').innerHTML = '<p>Fat: '
-                + document.getElementById('fat').value + '</p>';
+                carbs   = parseFloat(document.getElementById('carbs').value);
+                protein = parseFloat(document.getElementById('protein').value);
+                fat     = parseFloat(document.getElementById('fat').value);
+                cal     = parseFloat(document.getElementById('cal').value);
+                carbs   = (carbs * caloriesPerGram.carbs / cal);
+                protein = (protein * caloriesPerGram.protein / cal);
+                fat     = (fat * caloriesPerGram.fat / cal);
+                carbs   /= (maxIntake.carbs);
+                protein /= (maxIntake.protein);
+                fat     /= (maxIntake.fat);
+                carbs   = Math.round(carbs * 255)
+                protein = Math.round(protein * 255)
+                fat     = Math.round(fat * 255)
+                document.getElementById('data').innerHTML = '<p>' + carbs
+                        + ' ' + protein + ' ' + fat + '</p>';
 }, 100);
