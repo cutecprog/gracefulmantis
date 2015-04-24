22bdaea10f87665a1dc89bb87d5ce931eb71f9ac
cutecprog <andi.grooms@gmail.com>
2015-04-23 23:36:09 -0500

    Dick around with carbs, protein, fat value processing until complete diets look somewhat apealling

--- diet.js
+++ diet.js
@@ -9,6 +9,13 @@ var maxIntake = {
         'protein': 0.35,
         'fat':     0.35
 };
+
+var minIntake = {
+        'carbs':   0.45,
+        'protein': 0.10,
+        'fat':     0.20
+};
+
 var carbs, protein, fat, cal;
 document.getElementById('carbs').value = '0';
 document.getElementById('protein').value = '0';
@@ -27,15 +34,29 @@ setInterval(function()
                 carbs   = (carbs * caloriesPerGram.carbs / cal);
                 protein = (protein * caloriesPerGram.protein / cal);
                 fat     = (fat * caloriesPerGram.fat / cal);
-                carbs   /= (maxIntake.carbs);
-                protein /= (maxIntake.protein);
-                fat     /= (maxIntake.fat);
+                carbs   -= (minIntake.carbs);
+                protein -= (minIntake.protein);
+                fat     -= (minIntake.fat);
+                carbs   /= (maxIntake.carbs - minIntake.carbs);
+                protein /= (maxIntake.protein - minIntake.protein);
+                fat     /= (maxIntake.fat - minIntake.fat);
+                r = carbs;
+                g = protein;
+                b = fat;
+                document.getElementById('data').innerHTML = 
+                        '<p>'+r+' '+g+' '+b+'</p>';
+                r *= 256;
+                g *= 256;
+                b *= 256;
                 /* carbs:   yellow
                    protein: magenta
-                   fat:     cyan     */
-                r  = carbs * 127 + protein * 127;
-                g = carbs * 127 + fat * 127;
-                b = protein * 127 + fat * 127;
+                   fat:     cyan     
+                r  = Math.sqrt((Math.pow(carbs,2) + Math.pow(protein,2))/2);
+                g = Math.sqrt((Math.pow(carbs,2) + Math.pow(fat,2))/2);
+                b = Math.sqrt((Math.pow(protein,2) + Math.pow(fat,2))/2);*/
+                r = (r > 255) ? 255 : (r < 0) ? 0 : r;
+                g = (g > 255) ? 255 : (g < 0) ? 0 : g;
+                b = (b > 255) ? 255 : (b < 0) ? 0 : b;
                 r = Math.round(r).toString(16);
                 g = Math.round(g).toString(16);
                 b = Math.round(b).toString(16);
