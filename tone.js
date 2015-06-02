a94b1050c75c273796df5907246ccc04bf5f5df8
cutecprog <andi.grooms@gmail.com>
2015-06-01 19:28:41 -0500

    Add sample code for riffwave.js file to tone.js file

--- tone.js
+++ tone.js
@@ -1,4 +1,6 @@
-/*var data = [];
-for (var i=0; i < 10000; i++)
-        data[i] = Math.Round(255 * Math.random());*/
-alert("let's start this up");
+var data = [];                       // just an array
+for (var i=0; i<10000; i++)          // fill data with random samples
+        data[i] = Math.round(255 * Math.random()); 
+var wave = new RIFFWAVE(data);       // create the wave file
+var audio = new Audio(wave.dataURI); // create the HTML5 audio element
+audio.play();                        // some noise
