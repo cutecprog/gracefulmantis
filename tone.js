14cab607008fa557a4a72236aa51b3484d155386
cutecprog <andi.grooms@gmail.com>
2015-06-01 19:39:36 -0500

    Play foot step sound: set data to alternate ever 1000 index from a high tone to a low tone

--- tone.js
+++ tone.js
@@ -1,6 +1,9 @@
-var data = [];                       // just an array
-for (var i=0; i<10000; i++)          // fill data with random samples
-        data[i] = Math.round(255 * Math.random()); 
+var data = [];
+for (var i=0; i<100000; i++)
+        if (Math.round(i/1000)%2)
+                data[i] = 212;
+        else
+                data[i] = 128; 
 var wave = new RIFFWAVE(data);       // create the wave file
 var audio = new Audio(wave.dataURI); // create the HTML5 audio element
-audio.play();                        // some noise
+audio.play();
