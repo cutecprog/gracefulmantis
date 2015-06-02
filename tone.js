9bd3a39ad6cd569f0ef4b1b940f3d7b8146246b8
cutecprog <andi.grooms@gmail.com>
2015-06-02 07:14:23 -0500

    Play first song: play fibo tone mod 8 to 256 indexes

--- tone.js
+++ tone.js
@@ -1,16 +1,16 @@
 var data = [];
 var a = 1;
 var b = 1;
-for (j=0; j < 16; j++) {
+for (j=0; j < 256; j++) {
         a = a + b;
         a ^= b;
         b ^= a;
         a ^= b;
-        for (var i=0; i<5000; i++)
+        for (var i=0; i<4000; i++)
                 if (Math.round(i/(b%8+1))%2)
-                        data[5000*j+i] = 255;
+                        data[4000*j+i] = 128;
                 else
-                        data[5000*j+i] = 100;
+                        data[4000*j+i] = 0 + Math.round(16*Math.random());
 }
 var wave = new RIFFWAVE(data);       // create the wave file
 var audio = new Audio(wave.dataURI); // create the HTML5 audio element
