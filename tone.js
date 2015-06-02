d887dbc8c9cd4f8a24fc8418c4cd83a51d7cfe13
cutecprog <andi.grooms@gmail.com>
2015-06-01 20:49:46 -0500

    Play first set of tones using fibo

--- tone.js
+++ tone.js
@@ -1,9 +1,17 @@
 var data = [];
-for (var i=0; i<100000; i++)
-        if (Math.round(i/1000)%2)
-                data[i] = 212;
-        else
-                data[i] = 128; 
+var a = 1;
+var b = 1;
+for (j=0; j < 16; j++) {
+        a = a + b;
+        a ^= b;
+        b ^= a;
+        a ^= b;
+        for (var i=0; i<5000; i++)
+                if (Math.round(i/b)%2)
+                        data[5000*j+i] = 255;
+                else
+                        data[5000*j+i] = 0;
+}
 var wave = new RIFFWAVE(data);       // create the wave file
 var audio = new Audio(wave.dataURI); // create the HTML5 audio element
 audio.play();
