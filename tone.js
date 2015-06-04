ca7b41b805bcc53eba76bf2149c5e2ebc03be011
cutecprog <andi.grooms@gmail.com>
2015-06-04 13:29:42 -0500

    Play a twelve tone melody

--- tone.js
+++ tone.js
@@ -7,14 +7,14 @@ scale.sort(function() {
 
 addTone = function(data, n, length)
 {
-        for (i=data.length; i<length; i++) {
+        for (var i=data.length; i<length; i++) {
                 var step = Math.PI/Math.sqrt(n);
                 data[i] = Math.round(127.5*Math.sin(i * step)+127.5);
         }
 }
 
-for (j=0; j <= 128; j++) {
-        var n = Math.round( 12 * Math.random()) + 4;
+for (var i=0; i<12; i++) {
+        var n = scale[i];
         var length = data.length+2000;
         addTone(data, n, length);
 }
