91e518aa537f9d2456afa19e6b996edfc4bc59c8
cutecprog <andi.grooms@gmail.com>
2015-06-04 12:29:40 -0500

    Play sin wave instead of just high and low

--- tone.js
+++ tone.js
@@ -2,13 +2,9 @@ var data = [];
 
 for (j=0; j <= 128; j++) {
         for (i=2000*j; i<2000*(j+1); i++) {
-                if (i%j < j/2)
-                        data[i] = 0;
-                else {
-                        x = i % 256;
-                        data[i] = Math.round( 127.5 * Math.cos( 
-                                        Math.PI*x/1000+Math.PI)+1);
-                }
+                step = Math.PI/50;
+                data[i] = Math.round(127*Math.sin(i * step)+127);
+                //alert(data[i]);
         }
 }
 
