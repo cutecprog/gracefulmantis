399935563954d0c074ba6b3f57a709cbcaa7c381
cutecprog <andi.grooms@gmail.com>
2015-06-05 07:58:42 -0500

    Convert data list and addTone utility function into a singleton and method

--- tone.js
+++ tone.js
@@ -1,15 +1,19 @@
-var data = [];
 var scale = [4,5,6,7,8,9,10,11,12,13,14,15,16];
 
-
-addTone = function(data, n, length)
+data =
 {
-        length += data.length;
-        for (var i=data.length; i<length; i++) {
-                var step = Math.PI/Math.sqrt(n);
-                data[i] = Math.round(127.5*Math.sin(i * step)+127.5);
+        raw: [],
+        addTone: function(n, length)
+        {
+                length += this.raw.length;
+                for (var i=this.raw.length; i<length; i++) {
+                        var step = Math.PI/Math.sqrt(n);
+                        this.raw[i] = Math.round( 127.5*Math.sin(i * step)
+                                                + 127.5 );
+                }
         }
 }
+
 for (var j=0; j<32; j++) {
         scale.sort(function() {
                 return .5 - Math.random();
@@ -17,9 +21,9 @@ for (var j=0; j<32; j++) {
         for (var i=0; i<12; i++) {
                 var n = scale[i];
                 var length = 2000;
-                addTone(data, n, length);
+                data.addTone(n, length);
         }
 }
-var wave = new RIFFWAVE(data);       // create the wave file
+var wave = new RIFFWAVE(data.raw);       // create the wave file
 var audio = new Audio(wave.dataURI); // create the HTML5 audio element
 audio.play();
