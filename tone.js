63b26002ebc9e92ccf203868cda79b0c84f1c0bb
cutecprog <andi.grooms@gmail.com>
2015-06-04 20:37:53 -0500

    I hope you like it

--- tone.js
+++ tone.js
@@ -1,9 +1,6 @@
 var data = [];
 var scale = [4,5,6,7,8,9,10,11,12,13,14,15,16];
 
-scale.sort(function() {
-  return .5 - Math.random();
-});
 
 addTone = function(data, n, length)
 {
@@ -12,13 +9,16 @@ addTone = function(data, n, length)
                 data[i] = Math.round(127.5*Math.sin(i * step)+127.5);
         }
 }
-
-for (var i=0; i<12; i++) {
-        var n = scale[i];
-        var length = data.length+2000;
-        addTone(data, n, length);
+for (var j=0; j<32; j++) {
+        scale.sort(function() {
+                return .5 - Math.random();
+        });
+        for (var i=0; i<12; i++) {
+                var n = scale[i];
+                var length = data.length+2000;
+                addTone(data, n, length);
+        }
 }
-
 var wave = new RIFFWAVE(data);       // create the wave file
 var audio = new Audio(wave.dataURI); // create the HTML5 audio element
 audio.play();
