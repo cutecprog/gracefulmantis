a6337597075625513b7c0d4331f9a55ba246c006
cutecprog <andi.grooms@gmail.com>
2015-06-05 08:26:10 -0500

    Initialize, define and test insertTone method

--- tone.js
+++ tone.js
@@ -6,15 +6,28 @@ data =
         addTone: function(n, length)
         {
                 length += this.raw.length;
-                for (var i=this.raw.length; i<length; i++) {
+                for (var i=this.raw.length; i < length; i++) {
                         var step = Math.PI/Math.sqrt(n);
                         this.raw[i] = Math.round( 127.5*Math.sin(i * step)
                                                 + 127.5 );
                 }
+        },
+        insertTone: function(position, n, length)
+        {
+                length += position; 
+                for (var i=this.raw.length; i < length; i++) {
+                        this.raw[i] = 0;
+                }
+                for (var i=position; i < length; i++) {
+                        var step = Math.PI/Math.sqrt(n);
+                        this.raw[i] = Math.min( this.raw[i] + Math.round(
+                                                127.5*Math.sin(i * step)
+                                                + 127.5), 255);
+                }
         }
 }
 
-for (var j=0; j<32; j++) {
+/*for (var j=0; j<32; j++) {
         scale.sort(function() {
                 return .5 - Math.random();
         });
@@ -23,7 +36,19 @@ for (var j=0; j<32; j++) {
                 var length = 2000;
                 data.addTone(n, length);
         }
+}*/
+for (var j=0; j<32; j++) {
+        scale.sort(function() {
+                return .5 - Math.random();
+        });
+        for (var i=0; i<12; i++) {
+                var n = scale[i];
+                var length = 2000;
+                data.insertTone(j*24000+i*2000, n, length);
+        }
 }
+alert(data.raw);
+
 var wave = new RIFFWAVE(data.raw);       // create the wave file
 var audio = new Audio(wave.dataURI); // create the HTML5 audio element
 audio.play();
