633dcee674ec0453ab915e6b622229396e5b45db
cutecprog <andi.grooms@gmail.com>
2015-06-14 16:13:33 -0500

    Generate audio data to test smoothess with speakers

--- tone.js
+++ tone.js
@@ -60,7 +60,7 @@ var scale = [0,1,2,3,4,5,6,7,8,9,10,11];
 for (var j=0; j < 12; j++) {
         scale = shuffled(scale);
         for (var i=0; i < 12; i++) {
-                n = scale[i] - 24;
+                n = scale[i] - 4;
                 data.addTone(twelveTone(n), 500);
         }
 }
